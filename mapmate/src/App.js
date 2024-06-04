b3506e6edf1885aa3f2e0d65111c7572b9d32c61
unknown <gymdong@naver.com>
2024-06-05 00:19:28 +0900

    test commit

--- mapmate/src/App.js
+++ mapmate/src/App.js
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import config from "./apikey.js";
+
+const { kakao } = window;
 function App() {
-  return <div className="App">hello world</div>;
+  const key = config.KAKAO_API_KEY;
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+  return (
+    <div>
+      <div id="container" style={{ width: "100%", height: "100%" }}>
+        <p style={{ fontSize: 50, textAlign: "center" }}>Sample Page</p>
+        <div id="map" style={{ width: 500, height: 400 }}></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

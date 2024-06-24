df3def7e67fe1778c622c9763fe7c961bc60826d
Oh min jun <2023782@donga.ac.kr>
2024-06-24 10:56:36 +0900

    bio만 바꿨을때 적용안되던 버그 수정

--- mapmate/src/routes/Editprofile.js
+++ mapmate/src/routes/Editprofile.js
@@ -6,24 +6,48 @@ import { authService } from "fbase";
 import { dbService } from "fbase";
 
 function EditProfile({ onClose, changedUser }) {
-  const [bio, setBio] = useState("간략한 자기소개를 적어주세요!"); //디폴트 바이오 변경값
+  const [bio, setBio] = useState("간략한 자기소개를 적어주세요!"); // 디폴트 바이오 변경값
   const [avatar, setAvatar] = useState("");
 
   const handleBioChange = (event) => {
     setBio(event.target.value);
   };
+
   const handleSubmitAvatar = async (event) => {
-    //프로필사진 변경시 DB에 저장하는 함수
-    const attachmentRef = storageService
-      .ref()
-      .child(`${authService.currentUser.uid}/${uuidv4()}`);
-    console.log(avatar);
-    if (avatar == "") {
-      console.log("not changed");
-      onClose();
+    if (avatar === "") {
+      // 아바타가 변경되지 않은 경우 bio만 업데이트되게끔
+      dbService
+        .collection("user_info")
+        .where("user_id", "==", authService.currentUser.uid)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            dbService
+              .collection("user_info")
+              .doc(doc.id)
+              .update({
+                user_bio: bio,
+              })
+              .then(() => {
+                console.log("바이오 업데이트 성공");
+                changedUser(bio);
+                onClose();
+              })
+              .catch((error) => {
+                console.error("바이오 업데이트 오류:", error);
+              });
+          });
+        })
+        .catch((error) => {
+          console.error("문서 가져오기 오류:", error);
+        });
       return;
     }
 
+    // 아바타가 변경된 경우
+    const attachmentRef = storageService
+      .ref()
+      .child(`${authService.currentUser.uid}/${uuidv4()}`);
     const response = await attachmentRef.putString(avatar, "data_url");
     const attachmentUrl = await response.ref.getDownloadURL();
 
@@ -33,7 +57,6 @@ function EditProfile({ onClose, changedUser }) {
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          // 해당 문서의 ref를 가져와서 업데이트
           dbService
             .collection("user_info")
             .doc(doc.id)
@@ -42,24 +65,21 @@ function EditProfile({ onClose, changedUser }) {
               user_bio: bio,
             })
             .then(() => {
-              console.log("프로필 사진 업데이트 성공");
+              console.log("프로필 사진 및 바이오 업데이트 성공");
               changedUser(bio);
               onClose();
             })
             .catch((error) => {
-              console.error("프로필 사진 업데이트 오류:", error);
+              console.error("프로필 사진 및 바이오 업데이트 오류:", error);
             });
         });
       })
       .catch((error) => {
-        console.error("Error getting documents: ", error);
+        console.error("문서 가져오기 오류:", error);
       });
-    //console.log(response);
-
-    // window.location.reload("/profile");
   };
+
   const handleAvatarChange = async (event) => {
-    //미리보기를 위해
     const {
       target: { files },
     } = event;
@@ -98,7 +118,6 @@ function EditProfile({ onClose, changedUser }) {
                   type="file"
                   onChange={handleAvatarChange}
                   accept="image/*"
-                  required
                 />
               </label>
             </div>
